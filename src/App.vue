f179bdd02f63d4362dbff2187ee9a73552851d9e
Abdullah Nadeem <139441028+Muhmmad-Abdullah@users.noreply.github.com>
2023-08-03 19:30:13 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Fetch Data using Vue </h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.id }} - {{ user.name }} - {{ user.education }} - {{ user.email }} - {{ user.address }} - {{ user.number }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    this.fetchUsers();
+  },
+  methods: {
+    async fetchUsers() {
+      try {
+        const response = await axios.get("http://localhost:8080");
+        this.users = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
